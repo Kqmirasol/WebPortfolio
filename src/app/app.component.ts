d70d3e101d1b33f3ab0a2db746e9f993872ee524
Kqmirasol <kqmirasol@icloud.com>
2025-01-31 23:56:50 +0800

    Updated design and added contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { trigger, transition, style, animate, query, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('300ms ease-out', style({ opacity: 0, transform: 'translateY(-10px)' }))
+          ], { optional: true }),
+
+          query(':enter', [
+            style({ opacity: 0, transform: 'translateY(10px)' }),
+            animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ],
 })
 export class AppComponent {
   title = 'Karlo | Portfolio';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
